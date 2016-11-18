1796413f69b4e39b3371b273177aabf099e3cfb8
speane <avtolubitel225@gmail.com>
2016-11-18 03:48:08 +0300

    Moved rendering to renderer class

--- core/src/com/speanegames/fairybattles/screens/BattleFieldScreen.java
+++ core/src/com/speanegames/fairybattles/screens/BattleFieldScreen.java
@@ -2,12 +2,25 @@ package com.speanegames.fairybattles.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
 import com.speanegames.fairybattles.FairyBattlesGame;
+import com.speanegames.fairybattles.entities.Hero;
+import com.speanegames.fairybattles.rendering.Renderer;
 
 public class BattleFieldScreen extends ScreenAdapter {
 
     private final FairyBattlesGame game;
+    private final Renderer renderer;
+
+    private Hero hero;
 
     public BattleFieldScreen(FairyBattlesGame game) {
         this.game = game;
+        this.renderer = new Renderer();
+        this.hero = new Hero();
+    }
+
+    @Override
+    public void render(float delta) {
+        renderer.renderWorld();
+
     }
 }
